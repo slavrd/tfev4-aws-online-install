55a108ebeccf5727d1f65e7aab20e749a1be8a06
Alvaro Miranda Aguilera <kikitux@gmail.com>
2020-10-16 09:44:16 +0200

    refactor

--- ext-services/output.tf
+++
@@ -1,8 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.tfe_s3_bucket.id
-}
-
-output "pg_address" {
-  description = "The hostname of the PostgreSQL instance."
-  value       = aws_db_instance.tfe.address
-}
--- ssh-hop/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance."
-  value       = var.enable ? aws_instance.ssh_hop[0].public_ip : ""
-}
-
-output "public_dns" {
-  description = "Public DNS of the EC2 instance."
-  value       = var.enable ? aws_instance.ssh_hop[0].public_dns : ""
-}
