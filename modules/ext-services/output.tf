c7ed9e3d6d884ace0c00f2cd15142b98fb9b2180
Slav Donchev <slav.donchev@gmail.com>
2020-10-16 12:06:17 +0300

    make ext-services output the s3 bucket region

--- modules/ext-services/output.tf
+++ modules/ext-services/output.tf
@@ -1,5 +1,11 @@
 output "s3_bucket_name" {
+  description = "The name of the bucket."
   value = aws_s3_bucket.tfe_s3_bucket.id
+}
+
+output "s3_bucket_region" {
+  description = "The AWS region the bucket resides in."
+  value = aws_s3_bucket.tfe_s3_bucket.region
 }
 
 output "pg_address" {
